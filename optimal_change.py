b23b84c7200607f0a100a5b9f706b122fef73a80
BLMELVIN <bmelvin@alumni.berklee.edu>
2022-10-24 19:12:45 -0400

    assessment 1 solution

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,52 @@
 # Write your solution here!
+def optimal_change(item_cost, amount_paid):
+  
+    if type(item_cost) == str or type(amount_paid) == str:
+        return ('invalid input')
+    if item_cost > amount_paid:
+        return 'You did not pay enough for the Item!'
+    if item_cost < 0 or amount_paid < 0:
+        return 'Invalid Input'
+    list_of_bills = []
+    return_change_counter = {}
+    change_dictionary = {
+        '$100 bill' : 10000,
+        '$50 bill' : 5000,
+        '$20 bill' : 2000,
+        '$10 bill' : 1000,
+        '$5 bill' : 500,
+        '$1 bill' : 100,
+        'quarter' : 25,
+        'dime' : 10,
+        'nickel' : 5,
+        'penny' : 1
+    }
+    total_change = amount_paid - item_cost
+    total_change_to_pennies = round(float(total_change * 100))
+    for denomination in change_dictionary:
+        while total_change_to_pennies >= change_dictionary[denomination]:
+            list_of_bills.append(denomination)
+            total_change_to_pennies = total_change_to_pennies - change_dictionary[denomination]
+            if total_change_to_pennies == 0:
+                break 
+    if len(list_of_bills) == 0:
+       return f'The optimal change for an item that costs {item_cost} with an amount paid of {amount_paid} is no change is required.'        
+    for bill in list_of_bills:
+        return_change_counter[bill] = 0
+    for bill in list_of_bills:
+        return_change_counter[bill] += 1
+    retured_change_string = ''
+    the_key = ''
+    for bill_type in return_change_counter:
+        the_key = bill_type
+        if return_change_counter[bill_type] != 1:
+            the_key = the_key + 's'
+        if the_key == 'pennys':
+            the_key = 'pennies'  
+        retured_change_string += (f'{return_change_counter[bill_type]} {the_key}'+ ', ')
+    corrected_string = retured_change_string[:-2]
+    correct_change = (f"The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is {corrected_string}.")
+    reversed_string = correct_change[::-1].replace(",", 'dna ,', 1)
+    correct_change =reversed_string[::-1]
+    return correct_change
+
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,35 @@
 # Write your tests here!
+from optimal_change import optimal_change
+
+print("1:", optimal_change(62.13, 100) == "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+
+print("2:", optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+
+print('3:', optimal_change(99, 100) ==  'The optimal change for an item that costs $99 with an amount paid of $100 is 1 $1 bill.')
+
+print('4:', optimal_change(.75, 1) == 'The optimal change for an item that costs $0.75 with an amount paid of $1 is 1 quarter.')
+
+print('5:', optimal_change(0, 10000) == 'The optimal change for an item that costs $0 with an amount paid of $10000 is 100 $100 bills.')
+
+print('6:', optimal_change(1.24, 100) == 'The optimal change for an item that costs $1.24 with an amount paid of $100 is 1 $50 bill, 2 $20 bills, 1 $5 bill, 3 $1 bills, 3 quarters, and 1 penny.')
+
+print('7:', optimal_change(49.99, 100) == 'The optimal change for an item that costs $49.99 with an amount paid of $100 is 1 $50 bill, and 1 penny.')
+
+print('8:', optimal_change(100, 100) == 'The optimal change for an item that costs 100 with an amount paid of 100 is no change is required.')
+
+print('9:',  optimal_change(101, 100) == 'You did not pay enough for the Item!')
+
+print('10:', optimal_change(0, 0) == "The optimal change for an item that costs 0 with an amount paid of 0 is no change is required.")
+
+print('11:', optimal_change(9.91, 10.90) == 'The optimal change for an item that costs $9.91 with an amount paid of $10.9 is 3 quarters, 2 dimes, and 4 pennies.')
+
+print('12:', optimal_change(9.91, 10) == 'The optimal change for an item that costs $9.91 with an amount paid of $10 is 1 nickel, and 4 pennies.')
+
+print('13:', optimal_change('54', .67)== 'invalid input')
+
+print('14:', optimal_change(-1, 4) == 'Invalid Input')
+
+print('15:', optimal_change(1.37, -4) == 'You did not pay enough for the Item!')
+
+print(optimal_change(54.45, 600))
+print(optimal_change(56, 987))
